ca4101347e0697dd8a34444d323f5c1372c008ba
PZrobek <37156@studentwsei.pl>
2022-05-24 19:11:35 +0200

    Lab1 - Zastosowanie metod print, println

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,31 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+
+/**
+ * Komentarz javadoc w kodzie Ÿród³owym
+ *
+ * @author Piotr róbek
+ * @version Lab1
+ */
+public class Main {
+    public static void main(String[] args) {
+        //Komenda wypisuj¹ca tekst na ekranie
+        System.out.println("Hello Java World\n");
+
+    /*
+    kilka razy u¿yæ print
+    to samo, z u¿yciem println
+    wróciæ do print, ale u¿yæ znacznika koñca linii
+    */
+        System.out.print("Ala");
+        System.out.print("ma");
+        System.out.print("kota");
+
+        System.out.print("\n");
+
+        System.out.print("Ala\n");
+        System.out.print("ma\n");
+        System.out.print("kota\n");
+    }
+}
+
