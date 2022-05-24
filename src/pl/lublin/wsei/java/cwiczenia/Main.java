3851760475762b017410c07476da20da670b423a
PZrobek <37156@studentwsei.pl>
2022-05-24 19:35:25 +0200

    Lab1 - •	Deklaracja trzech zmiennych typów (int, double, string) i wypisanie ich wartości za pomocą metody printf

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -9,23 +9,30 @@ package pl.lublin.wsei.java.cwiczenia;
  */
 public class Main {
     public static void main(String[] args) {
-        //Komenda wypisuj¹ca tekst na ekranie
-        System.out.println("Hello Java World\n");
+//        //Komenda wypisuj¹ca tekst na ekranie
+//        System.out.println("Hello Java World\n");
+//
+//    /*
+//    kilka razy u¿yæ print
+//    to samo, z u¿yciem println
+//    wróciæ do print, ale u¿yæ znacznika koñca linii
+//    */
+//        System.out.print("Ala");
+//        System.out.print("ma");
+//        System.out.print("kota");
+//
+//        System.out.print("\n");
+//
+//        System.out.print("Ala\n");
+//        System.out.print("ma\n");
+//        System.out.print("kota\n");
 
-    /*
-    kilka razy u¿yæ print
-    to samo, z u¿yciem println
-    wróciæ do print, ale u¿yæ znacznika koñca linii
-    */
-        System.out.print("Ala");
-        System.out.print("ma");
-        System.out.print("kota");
+// zadeklarowaæ trzy zmienne typów int, double i string i wypisaæ ich wartoœæ za pomoc¹ printf;
+        int a = 3;
+        double b = 4.21;
+        String s = "jakiœ tekst";
+        System.out.printf("a = %d, b = %.2f, s = %20s %n",a,b,s);
 
-        System.out.print("\n");
-
-        System.out.print("Ala\n");
-        System.out.print("ma\n");
-        System.out.print("kota\n");
     }
 }
 
